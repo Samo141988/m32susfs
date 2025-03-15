a196eb0492c639d34f9ee2461e63a3ee54a1be3d
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2025-03-16 02:17:16 +0300

    Update gt1x_tpd.c

--- drivers/input/touchscreen/mediatek/GT5688/gt1x_tpd.c
+++ drivers/input/touchscreen/mediatek/GT5688/gt1x_tpd.c
@@ -666,7 +666,7 @@ static s32 tpd_i2c_probe(struct i2c_client *client,
 
 	GTP_INFO("%s start.", __func__);
 #ifdef CONFIG_MTK_BOOT
-	if (get_boot_mode() == RECOVERY_BOOT)
+	if (get_boot_mode() == NORMAL_BOOT)
 		return 0;
 #endif
 	probe_thread =
@@ -779,7 +779,7 @@ void gt1x_touch_down(s32 x, s32 y, s32 size, s32 id)
 #ifdef CONFIG_MTK_BOOT
 	if (tpd_dts_data.use_tpd_button) {
 		if (get_boot_mode() == FACTORY_BOOT ||
-		    get_boot_mode() == RECOVERY_BOOT)
+		    get_boot_mode() == NORMAL_BOOT)
 			tpd_button(x, y, 1);
 	}
 #endif
@@ -801,7 +801,7 @@ void gt1x_touch_up(s32 id)
 #ifdef CONFIG_MTK_BOOT
 	if (tpd_dts_data.use_tpd_button) {
 		if (get_boot_mode() == FACTORY_BOOT ||
-		    get_boot_mode() == RECOVERY_BOOT)
+		    get_boot_mode() == NORMAL_BOOT)
 			tpd_button(0, 0, 0);
 	}
 #endif
