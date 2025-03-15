570e8bb0d628d279473bafe887ba2fecb2ec3eec
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2025-03-16 02:29:19 +0300

    Update gt1x_tpd.c

--- drivers/input/touchscreen/mediatek/GT1151/gt1x_tpd.c
+++ drivers/input/touchscreen/mediatek/GT1151/gt1x_tpd.c
@@ -658,7 +658,7 @@ static s32 tpd_i2c_probe(struct i2c_client *client,
 
 	GTP_INFO("%s start.", __func__);
 #ifdef CONFIG_MTK_BOOT
-	if (get_boot_mode() == RECOVERY_BOOT)
+	if (get_boot_mode() == NORMAL_BOOT)
 		return 0;
 #endif
 	probe_thread = kthread_run(tpd_registration,
@@ -759,8 +759,8 @@ void gt1x_touch_down(s32 x, s32 y, s32 size, s32 id)
 	tpd_history_y = y;
 #ifdef CONFIG_MTK_BOOT
 	if (tpd_dts_data.use_tpd_button) {
-		if (get_boot_mode() == FACTORY_BOOT ||
-			get_boot_mode() == RECOVERY_BOOT)
+		if (get_boot_mode() == NORMAL_BOOT ||
+			get_boot_mode() == NORMAL_BOOT)
 			tpd_button(x, y, 1);
 	}
 #endif
@@ -781,8 +781,8 @@ void gt1x_touch_up(s32 id)
 	tpd_history_y = 0;
 #ifdef CONFIG_MTK_BOOT
 	if (tpd_dts_data.use_tpd_button) {
-		if (get_boot_mode() == FACTORY_BOOT ||
-			get_boot_mode() == RECOVERY_BOOT)
+		if (get_boot_mode() == NORMAL_BOOT ||
+			get_boot_mode() == NORMAL_BOOT)
 			tpd_button(0, 0, 0);
 	}
 #endif
