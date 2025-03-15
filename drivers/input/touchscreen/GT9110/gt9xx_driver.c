8a9517f7f16ac5887296276432bf3d47ecb7f2b8
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2025-03-16 02:09:44 +0300

    Update gt9xx_driver.c

--- drivers/input/touchscreen/GT9110/gt9xx_driver.c
+++ drivers/input/touchscreen/GT9110/gt9xx_driver.c
@@ -2296,7 +2296,7 @@ static void tpd_down(s32 x, s32 y, s32 size, s32 id)
 	/* MMProfileLogEx(MMP_TouchPanelEvent, MMProfileFlagPulse, 1, x+y); */
 	if (tpd_dts_data.use_tpd_button) {
 		if (get_boot_mode() == FACTORY_BOOT ||
-		    get_boot_mode() == RECOVERY_BOOT)
+		    get_boot_mode() == NORMAL_BOOT)
 			tpd_button(x, y, 1);
 	}
 }
@@ -2316,7 +2316,7 @@ static void tpd_up(s32 x, s32 y, s32 id)
 
 	if (tpd_dts_data.use_tpd_button) {
 		if (get_boot_mode() == FACTORY_BOOT ||
-		    get_boot_mode() == RECOVERY_BOOT)
+		    get_boot_mode() == NORMAL_BOOT)
 			tpd_button(x, y, 0);
 	}
 }
